f31c249ff85bf97809786fc2a7bd5f48da849553
root <root@Antonshell.220v.ru>
2017-12-29 16:21:12 +0300

    added html design

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+//import logo from '../logo.svg';
+//import './App/App.css';
+
+class Header extends Component {
+    render() {
+        return (
+            <footer className="py-5 bg-dark">
+                <div className="container">
+                    <p className="m-0 text-center text-white">Copyright © Your Website 2017</p>
+                </div>
+            </footer>
+        );
+    }
+}
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import App from './components/App';
+//import './index.css';
+//import './../node_modules/bootstrap/dist/css/bootstrap.css';
+
+import { Route, Switch } from 'react-router-dom';
+import { ConnectedRouter } from 'react-router-redux';
+
+
+import 'bootstrap/dist/css/bootstrap.css'
 
 ReactDOM.render(
   <App />,
